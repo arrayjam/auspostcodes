de87b3afd29dfbd01f063a55692cd24832281636
Yuri Feldman <yuri@tbqh.net>
2016-08-08 17:09:03 +1000

    start re-typing turf

---
+++ src/precalculate_postcode_boundaries.ts
@@ -0,0 +1,45 @@
+/// <reference path="../typings/index.d.ts"/>
+
+import along from "turf-area";
+
+import { along as along2 } from "turf";
+// import { coordEach } from "turf-meta";
+
+// import * as shapefile from "shapefile";
+
+// shapefile.read(process.argv[2], (error, postalGeoJSON) => {
+
+
+// var bounds = [];
+// geojson.features.forEach(function(feature) {
+//   var props = feature.properties;
+//   var geometry = feature.geometry;
+
+//   if (geometry) {
+//     var e = extent(feature);
+//     module.exports = function(layer) {
+//     var extent = [Infinity, Infinity, -Infinity, -Infinity];
+//     each(layer, function(coord) {
+//       if (extent[0] > coord[0]) extent[0] = coord[0];
+//       if (extent[1] > coord[1]) extent[1] = coord[1];
+//       if (extent[2] < coord[0]) extent[2] = coord[0];
+//       if (extent[3] < coord[1]) extent[3] = coord[1];
+//     });
+//     return extent;
+// };
+
+//     var result = {
+//       // NOTE(yuri): Convert from WSEN to [[W, N], [E, S]]
+//       bounds: [[ e[0], e[3] ], [ e[2], e[1] ]],
+//       postcode:    props.postcode,
+//       digit_one:   props.digit_one,
+//       digit_two:   props.digit_two,
+//       digit_three: props.digit_three,
+//       digit_four:  props.digit_four,
+//     };
+
+//     bounds.push(result);
+//   }
+// });
+
+// process.stdout.write(JSON.stringify(bounds));
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,3 +1,4 @@
 /// <reference path="globals/geojson/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
 /// <reference path="globals/shapefile/index.d.ts" />
+/// <reference path="globals/turf/index.d.ts" />
